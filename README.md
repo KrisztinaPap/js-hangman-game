323e9a7263562e39ff39796f440f82640c4c506d
Christine Echo <christineecho@outlook.com>
2020-08-26 16:33:22 -0600

    Adds an array of words as wordArray; Adds function to provide randomNumber; Adds feature to choose a random secretWord; Adds feature that provides secretWordUnderscore (underscores the sme number as the secret word;

--- README.md
+++ README.md
@@ -11,7 +11,8 @@ Krisztina Pap
 August 26, 2020
 
 ## Resources
-[Normalize.css](https://cssreset.com/scripts/normalize-css/)
+- [Normalize.css](https://cssreset.com/scripts/normalize-css/)
+- [GeeksForGeeks: How to generate random number in given range using JavaScript?](https://www.geeksforgeeks.org/how-to-generate-random-number-in-given-range-using-javascript/)
 
 ## Trello Project Board
 https://trello.com/b/VcXfo0sg/js-hangman
--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,24 @@
 // on document load -   secretWord is chosen randomly from wordArray
 //                      Underscores are displayed to represent length of SecretWord
 //                      number of chances (integer) is set to starting value
+const wordArray = [ "elephant", "automobile", "vegetable", "waterfall", "elementary", "pseudocode", "pharmacy", "thunder" ];
+
+let guessChances = 6;
+
+// Citation:
+//      https://www.geeksforgeeks.org/how-to-generate-random-number-in-given-range-using-javascript/
+//      The below function works as follows: Math.random() provides a random number between 0 and 1. It then multiplies that by the number of array items and rounds it down to get a number between 0 and the number of items in the array.
+function findRandomNumber (min, max) {
+    return Math.floor(Math.random() * (max-min));
+}
+let randomNumber = findRandomNumber( 0, wordArray.length );
+let secretWord = wordArray[randomNumber];
+let secretWordUnderscore = "";
+
+for ( let i = 0; i < secretWord.length; i++ ) {
+    secretWordUnderscore += "_";
+};
+console.log(secretWordUnderscore);
 
 // User enters a letter letterGuess
 
@@ -25,7 +43,7 @@
 // - previousGuesses = [];
 // - randomNumber (to select a random secretWord from the wordArray)
 // - secretWord = ""; (string, one of the words from wordArray)
-// - secretWordUnderscores = secretWordLength * underscores;
+// - secretWordUnderscore = secretWordLength * underscores;
 // - underscoreNumber (integer - the number of underscores left in the secretWord)
 // - guessChances = 6 (integer that counts down)
 
