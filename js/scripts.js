3ac7836189f3a48bddf7ab70fa1d1ade0e32434d
Christine Echo <christineecho@outlook.com>
2020-08-26 16:03:57 -0600

    Pseudocode added (will be removed when progrm is finished);

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+// Pseudo code - to be deleted when program is finished
+
+// on document load -   secretWord is chosen randomly from wordArray
+//                      Underscores are displayed to represent length of SecretWord
+//                      number of chances (integer) is set to starting value
+
+// User enters a letter letterGuess
+
+// If secretWord contains letterGuess, letter is displayed instead of underscores
+
+// ELSE -   letterGuess is added to wrongGuess array and gets added to Previous Guesses list on screen
+//          chances --
+
+// Breakout conditions   -  If number of underscores in secretWord == 0 (WIN)
+//                          If chances == 0 (LOSE)
+
+// Breakout action      -   Alert (win) OR alert (lose)
+
+// Ask if user wants to play again
+            
+
+// Variables planned:
+// - letterGuess = ""; string or char???? (User input (form, text)) 
+// - wordArray = [];
+// - previousGuesses = [];
+// - randomNumber (to select a random secretWord from the wordArray)
+// - secretWord = ""; (string, one of the words from wordArray)
+// - secretWordLength (integer)
+// - underscoreNumber (integer - the number of underscores left in the secretWord)
+// - guessChances = 6 (integer that counts down)
+
+// - win alert message
+// - lose alert message
